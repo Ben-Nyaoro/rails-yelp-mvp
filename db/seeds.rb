845a5ee5c1bfe1709af64ef8e01d39d9de0024c0
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-04-13 22:58:55 +0200

    Finished the project with all rails routes implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning the database"
+
+Restaurant.destroy_all
+
+puts "Creating new restaurants"
+
+50.times do
+ @restaurant = Restaurant.create!(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.street_address,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+)
+puts "created #{@restaurant.name}"
+end
+
+puts "done!!"
+
+
